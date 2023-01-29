2856dbbcfe9affb54b53ca0af3e9a523ef83aae8
Carpet <123202648+Carpetlord@users.noreply.github.com>
2023-01-29 18:15:14 +0100

    Delete Courses/Alchemy University/JS/JavaScriptBasics/Functions and Operators directory

--- Courses/Alchemy University/JS/JavaScriptBasics/Functions and Operators/2) Add Two/test.js
+++
@@ -1,8 +0,0 @@
-const addTwo = require('../addTwo');
-const {assert} = require('chai');
-
-it('should add two', () => {
-    assert.equal(addTwo(2), 4);
-    assert.equal(addTwo(6), 8);
-    assert.equal(addTwo(10), 12);
-});
